5d979a5c603116a2d6ed6afcf8e0c973419590c4
Joshua Muheim <josh@josh.ch>
2016-01-29 21:41:23 +0100

    Cleanup

--- spec/features/documents/destroy_spec.rb
+++ spec/features/documents/destroy_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'Deleting report' do
   before do
-    @document = create :document
+    @document = create :document, :with_image
     login_as(create :user)
   end
 
   it 'deletes a document' do
     expect {
       visit_delete_path_for(@document)
-    }.to change { Document.count }.by -1
+    }.to change { Document.count }
   end
 end
--- spec/features/documents/show_spec.rb
+++ spec/features/documents/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Showing document' do
   before do
-    @document = create :document
+    @document = create :document, :with_image
     login_as(create :admin)
   end
 
@@ -16,6 +16,7 @@ describe 'Showing document' do
     within dom_id_selector(@document) do
       expect(page).to have_css '.description', text: 'Document test description'
       expect(page).to have_css '.content',     text: 'Document test content'
+      expect(page).to have_css '.images a img'
       expect(page).to have_css '.created_at',  text: 'Mon, 15 Jun 2015 14:33:52 +0200'
       expect(page).to have_css '.updated_at',  text: 'Mon, 15 Jun 2015 14:33:52 +0200'
 
