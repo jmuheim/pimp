31297ae11f0cf7c391b97756dda68f75db140e54
Joshua Muheim <josh@josh.ch>
2016-01-30 20:07:28 +0100

    Specs

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,6 +1,6 @@
 # See http://stackoverflow.com/questions/30018652/slim-template-doesnt-render-markdown-stored-in-a-variable
 module MarkdownHelper
   def markdown(string)
-    PandocRuby.convert(string, to: :html).html_safe
+    PandocRuby.convert(string, to: :html).strip.html_safe
   end
 end
